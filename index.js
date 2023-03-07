935cb08e6f88e9a6219eb434f4486a43b3e5b915
yujun.xu <yujun.xu@linkflowtech.com>
2023-03-07 16:54:50 +0800

    支持跨域

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
-
 const app = express();
 
 // 使用h5 history模式需要载入 connect-history-api-fallback 中间件，用于单页面项目
@@ -19,6 +18,17 @@ app.use(history({
     ]
 }));
 app.use(express.static('dist'));
+
+// 自己实现的跨域中间件
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
+    next();
+});
 
 app.listen(8082, () => {
     console.log('express server running at http://127.0.0.1:8082')
